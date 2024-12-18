110c9395c9d3282665123248ac23023c6f95486e
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-12-18 20:07:09 -0300

    fix: correct import path for user controller in User.routes

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user.controller')
+const userController = require('../controllers/User.controller')
 const auth  = require('../middleware/Auth.middleware')
 
 // ==> Rota responsavel por criar o novo 'User': (POST) localhost:3000/api/v1/register
