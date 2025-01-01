afebc98d49e6aa2e140525bfca66a297e39ce3ec
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-01 15:38:49 -0300

    fix: update API route to root endpoint in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/api/v1', (req, res)=> {
+router.get('/', (req, res)=> {
     res.status(200).send({
         sucess: true,
         message: 'Seja bem-vindo(a) à nossa API',
