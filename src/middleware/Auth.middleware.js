c380aa8afaa1ce5dfc0e10403ea65e22bda71bc9
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-03 21:43:57 -0300

    fix: replace hardcoded secret with environment variable in Auth.middleware.js

--- src/middleware/Auth.middleware.js
+++ src/middleware/Auth.middleware.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
+dotenv.config();
 module.exports = (req, res, next)=>{
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if(token == null) return res.sendStatus(401); // Unauthorized
-    jwt.verify(token, 'secrete', (err, user) => {
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
         if(err) return res.sendStatus(403); // Forbidden
         req.userData = user;
         next();
