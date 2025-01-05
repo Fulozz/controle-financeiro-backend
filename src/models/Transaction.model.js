3a7203a635e8788d5ef2fa9a7f43fce2114822bc
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-05 18:06:10 -0300

    fix: add formaPagamento, parcelas, and descricao fields to transaction model

--- src/models/Transaction.model.js
+++ src/models/Transaction.model.js
@@ -5,9 +5,12 @@ const Schema = mongoose.Schema;
 const transactionSchema = new Schema({
     userID: { type: String, required: true},
     mesRef: { type: String, required: true},
+    formaPagamento: { type: String, required: true},
+    parcelas: { type: Number, required: false},
+    descricao: { type: String, required: true},
     valor: { type: Number, required: true},
     tipo: { type: String, required: true},
-    date: { type: Date,  required: true }
+    date: { type: String,  required: true }
 },{
     timestamps: true,
     collection: 'transactions'
