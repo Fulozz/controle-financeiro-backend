7c18f6fb054f48394e295ef3419ff10195a87a60
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-17 20:32:36 -0300

    fix: add titulo field and make formaPagamento and parcelas optional in transaction model

--- src/models/Transaction.model.js
+++ src/models/Transaction.model.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema({
     userID: { type: String, required: true},
+    titulo: { type: String, required: true},
     mesRef: { type: String, required: true},
-    formaPagamento: { type: String, required: true},
-    parcelas: { type: Number, required: false},
+    formaPagamento: { type: String},
+    parcelas: { type: Number},
     descricao: { type: String, required: true},
     valor: { type: Number, required: true},
     tipo: { type: String, required: true},
